14f59d700cebedac3ec3e4c30f960f62becfe92a
JOSH <sibendirejoshua@gmail.com>
2023-10-01 12:43:24 +0300

    created token

---
+++ src/main/java/com/registrationlogin/registration/appregistration/tokens/TokenConfirmation.java
@@ -0,0 +1,27 @@
+package com.registrationlogin.registration.appregistration.tokens;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@NoArgsConstructor
+public class TokenConfirmation {
+    private Long id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+    private LocalDateTime confirmedAt;
+
+    public TokenConfirmation(String token,
+                             LocalDateTime createdAt,
+                             LocalDateTime expiredAt,
+                             LocalDateTime confirmedAt) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiredAt = expiredAt;
+        this.confirmedAt = confirmedAt;
+    }
+}
